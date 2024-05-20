e2b0c051249d9f338e0a4f578e5bf739fa9c33c2
LY <liuyong235@huawei.com>
2024-05-20 12:33:04 +0000

    update icu4c/source/common/lstmbe.cpp.
    Signed-off-by: LY <liuyong235@huawei.com>

--- icu4c/source/common/lstmbe.cpp
+++ icu4c/source/common/lstmbe.cpp
@@ -554,7 +554,7 @@ GraphemeClusterVectorizer::~GraphemeClusterVectorizer()
 {
 }
 
-constexpr int32_t MAX_GRAPHEME_CLSTER_LENGTH = 12;
+constexpr int32_t MAX_GRAPHEME_CLSTER_LENGTH = 20;
 
 void GraphemeClusterVectorizer::vectorize(
     UText *text, int32_t startPos, int32_t endPos,
