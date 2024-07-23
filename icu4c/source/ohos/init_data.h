2a11d9754e1ec4a1eeb9587c397308d6504e0221
zhangdd_ewan <zhangdongdong50@huawei.com>
2024-07-23 15:13:07 +0800

    一致性修改
    Signed-off-by: zhangdd_ewan <zhangdongdong50@huawei.com>

--- icu4c/source/ohos/init_data.h
+++ icu4c/source/ohos/init_data.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,7 +22,7 @@
 /**
  * convienient method to set icu data file directory to HW_DIRECTORY);
  */
-void SetHwIcuDirectory( );
+void SetHwIcuDirectory();
 
 /**
  * get icu version
