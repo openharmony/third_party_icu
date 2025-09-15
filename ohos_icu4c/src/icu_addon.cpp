c28ef8e5bbd98726afe1716a308e7fdc507a0465
zhangdd_ewan <zhangdongdong50@huawei.com>
2025-09-15 21:52:04 +0800

    新增ndk接口
    Signed-off-by: zhangdd_ewan <zhangdongdong50@huawei.com>

--- ohos_icu4c/src/icu_addon.cpp
+++ ohos_icu4c/src/icu_addon.cpp
@@ -33,16 +33,6 @@
 #include <unicode/ustring.h>
 #include <unicode/utext.h>
 #include <unicode/utrans.h>
-#include <unicode/putil.h>
-#include <unicode/ucpmap.h>
-#include <unicode/uldnames.h>
-#include <unicode/ulocdata.h>
-#include <unicode/utypes.h>
-#include <unicode/uvernum.h>
-#include <unicode/uversion.h>
-#include <unicode/utf.h>
-#include <unicode/utf8.h>
-#include <unicode/utf16.h>
 
 #ifdef ICU_SUPPORT_LIBBEGETUTIL
 #include "parameter.h"
@@ -575,27 +565,6 @@ bool g_isCustomLocale = false;
 #undef u_strToUTF32WithSub
 #undef u_strFromUTF32WithSub
 #undef u_errorName
-#undef u_charAge
-#undef u_getUnicodeVersion
-#undef uldn_open
-#undef uldn_close
-#undef uldn_getLocale
-#undef uldn_getDialectHandling
-#undef uldn_localeDisplayName
-#undef uldn_languageDisplayName
-#undef uldn_scriptDisplayName
-#undef uldn_scriptCodeDisplayName
-#undef uldn_regionDisplayName
-#undef uldn_variantDisplayName
-#undef uldn_keyDisplayName
-#undef uldn_keyValueDisplayName
-#undef uldn_openForContext
-#undef uldn_getContext
-#undef ulocdata_getCLDRVersion
-#undef u_versionFromString
-#undef u_versionFromUString
-#undef u_versionToString
-#undef u_getVersion
 
 extern "C"
 {
@@ -2871,102 +2840,5 @@ UChar* u_strFromUTF32WithSub(UChar *dest, int32_t destCapacity, int32_t *pDestLe
 const char* u_errorName(UErrorCode code)
 {
     return U_ICU_ENTRY_POINT_RENAME(u_errorName)(code);
-}
-void u_charAge(UChar32 c, UVersionInfo versionArray)
-{
-    U_ICU_ENTRY_POINT_RENAME(u_charAge)(c, versionArray);
-}
-void u_getUnicodeVersion(UVersionInfo versionArray)
-{
-    U_ICU_ENTRY_POINT_RENAME(u_getUnicodeVersion)(versionArray);
-}
-ULocaleDisplayNames* uldn_open(const char *locale, UDialectHandling dialectHandling, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_open)(locale, dialectHandling, pErrorCode);
-}
-void uldn_close(ULocaleDisplayNames *ldn)
-{
-    U_ICU_ENTRY_POINT_RENAME(uldn_close)(ldn);
-}
-const char* uldn_getLocale(const ULocaleDisplayNames *ldn)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_getLocale)(ldn);
-}
-UDialectHandling uldn_getDialectHandling(const ULocaleDisplayNames *ldn)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_getDialectHandling)(ldn);
-}
-int32_t uldn_localeDisplayName(const ULocaleDisplayNames *ldn, const char *locale, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_localeDisplayName)(ldn, locale, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_languageDisplayName(const ULocaleDisplayNames *ldn, const char *lang, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_languageDisplayName)(ldn, lang, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_scriptDisplayName(const ULocaleDisplayNames *ldn, const char *script, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_scriptDisplayName)(ldn, script, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_scriptCodeDisplayName(const ULocaleDisplayNames *ldn, UScriptCode scriptCode, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_scriptCodeDisplayName)(ldn,
-        scriptCode, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_regionDisplayName(const ULocaleDisplayNames *ldn, const char *region, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_regionDisplayName)(ldn, region, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_variantDisplayName(const ULocaleDisplayNames *ldn, const char *variant, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_variantDisplayName)(ldn,
-        variant, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_keyDisplayName(const ULocaleDisplayNames *ldn, const char *key, UChar *result,
-    int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_keyDisplayName)(ldn, key, result, maxResultSize, pErrorCode);
-}
-int32_t uldn_keyValueDisplayName(const ULocaleDisplayNames *ldn, const char *key, const char *value,
-    UChar *result, int32_t maxResultSize, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_keyValueDisplayName)(ldn,
-        key, value, result, maxResultSize, pErrorCode);
-}
-ULocaleDisplayNames* uldn_openForContext(const char *locale, UDisplayContext *contexts,
-    int32_t length, UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_openForContext)(locale, contexts, length, pErrorCode);
-}
-UDisplayContext uldn_getContext(const ULocaleDisplayNames *ldn, UDisplayContextType type,
-    UErrorCode *pErrorCode)
-{
-    return U_ICU_ENTRY_POINT_RENAME(uldn_getContext)(ldn, type, pErrorCode);
-}
-void ulocdata_getCLDRVersion(UVersionInfo versionArray, UErrorCode *status)
-{
-    U_ICU_ENTRY_POINT_RENAME(ulocdata_getCLDRVersion)(versionArray, status);
-}
-void u_versionFromString(UVersionInfo versionArray, const char *versionString)
-{
-    U_ICU_ENTRY_POINT_RENAME(u_versionFromString)(versionArray, versionString);
-}
-void u_versionFromUString(UVersionInfo versionArray, const UChar *versionString)
-{
-    U_ICU_ENTRY_POINT_RENAME(u_versionFromUString)(versionArray, versionString);
-}
-void u_versionToString(const UVersionInfo versionArray, char *versionString)
-{
-    U_ICU_ENTRY_POINT_RENAME(u_versionToString)(versionArray, versionString);
-}
-void u_getVersion(UVersionInfo versionArray)
-{
-    U_ICU_ENTRY_POINT_RENAME(u_getVersion)(versionArray);
 }
 }
